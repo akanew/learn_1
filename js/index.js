14c5d58677d8a9d884a9e09b5bd90cd718b6463a
AkaNew <akanew@mail.ru>
2019-07-09 13:07:40 +0400

    Check supporting canvas at browser

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
-ctx.moveTo(50, 50);
-ctx.lineTo(200, 200);
-ctx.strokeStyle = "blue";
-ctx.stroke();
+
+if(ctx){
+	ctx.moveTo(50, 50);
+	ctx.lineTo(200, 200);
+	ctx.strokeStyle = "blue";
+	ctx.stroke();
+}
+else {
+	//browser don't support the canvas
+}
+
